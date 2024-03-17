ba9d649b77fb86aaa271ba154b80a80bf7d1209c
alaa <alaa@gmail.com>
2024-03-17 17:49:15 +0300

    Solitaire Cards

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/game_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: GameScreen(),
+    );
+  }
+}
